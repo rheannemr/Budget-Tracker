3c8c2d71e014a26f96d93e8b951c28f7a1d5f7f4
Rheanne Reyes <rheannemr@gmail.com>
2021-03-22 17:58:37 -0700

    Creating objects and transactions

--- public/db.js
+++ public/db.js
@@ -1,8 +1,11 @@
 let db;
-// create a new db request for a "budget" database.
+const request = indexedDB.open("budget", 1)
 
 request.onupgradeneeded = function (event) {
   // create object store called "pending" and set autoIncrement to true
+  // request.onupgradeneeded = ({ target }) => {
+  // const db = target.result;
+  const objectStore = db.createObjectStore("pending", { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
@@ -21,12 +24,20 @@ function saveRecord(record) {
   // create a transaction on the pending db with readwrite access
   // access your pending object store
   // add record to your store with add method.
-}
+  request.onsuccess = () => {
+    const db = request.result;
+    const transaction = db.transaction(["pending"], "readwrite");
+    const objectStore = transaction.objectStore("pending");
+    // const statusIndex = objectStore.index("statusIndex");
+    
+    // Adds data to our objectStore
+    objectStore.add({record});
 
 function checkDatabase() {
   // open a transaction on your pending db
   // access your pending object store
   // get all records from store and set to a variable
+  const transaction = db.transaction(["pending"], "readwrite");
 
   getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
@@ -41,6 +52,7 @@ function checkDatabase() {
         .then((response) => response.json())
         .then(() => {
           // if successful, open a transaction on your pending db
+          const transaction = db.transaction(["pending"], "readwrite");
           // access your pending object store
           // clear all items in your store
         });
